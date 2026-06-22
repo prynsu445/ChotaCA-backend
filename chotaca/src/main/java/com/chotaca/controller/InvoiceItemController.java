6f9899f7fc1a514ae91cbfdee347728cc3706cb4
Prynsu445 <priyanshusharma0142@gmail.com>
2026-06-22 16:34:23 +0530

    fic items

--- chotaca/src/main/java/com/chotaca/controller/InvoiceItemController.java
+++ chotaca/src/main/java/com/chotaca/controller/InvoiceItemController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/invoices-items")
+@RequestMapping("/api/items")
 public class InvoiceItemController {
 
     @Autowired
