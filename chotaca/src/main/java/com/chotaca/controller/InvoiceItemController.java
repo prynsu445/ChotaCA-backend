51cd3f88b943e1c1bba813527e87acd897de58e8
Prynsu445 <priyanshusharma0142@gmail.com>
2026-06-22 15:37:10 +0530

    fix the mapping

--- chotaca/src/main/java/com/chotaca/controller/InvoiceItemController.java
+++ chotaca/src/main/java/com/chotaca/controller/InvoiceItemController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/items")
+@RequestMapping("/api/invoices-items")
 public class InvoiceItemController {
 
     @Autowired
